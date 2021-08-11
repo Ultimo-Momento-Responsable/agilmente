28ea7ccf69469cee19ec15f6161535b4bd69d823
rassyu <andresmatter@hotmail.com>
2021-08-11 19:50:10 -0300

    :sparkles: Feat (main-menu): get planning added (Waiting for backend)

--- Assets/MainSceneController.cs
+++ Assets/MainSceneController.cs
@@ -1,25 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class MainSceneController : MonoBehaviour
 {
     public Text title;
-
+    public GameObject bodyText;
+    private Settings settings;
     public void btnGames()
     {
         title.text = "Jugar";
+        bodyText.SetActive(true);
+        bodyText.GetComponent<Text>().text = "Juegos pendientes"; 
         print("Boton jugar clickeado");
     }
     public void btnHome()
     {
         title.text = "Inicio";
+        bodyText.SetActive(true);
+        settings = JsonUtility.FromJson<Settings>(System.IO.File.ReadAllText(Application.dataPath + "/settings.json"));
+        bodyText.GetComponent<Text>().text = "¡Hola de nuevo " + settings.Login.patient.firstName + "!";
         print("Boton inicio clickeado");
     }
     public void btnProfile()
     {
         title.text = "Perfil";
+        bodyText.SetActive(false);
         print("Boton perfil clickeado");
     }
+
+    //GET PARA PLANNING DESDE BACKEND - 
+    public void getPlanning()
+    {
+        this.StartCoroutine(this.getPlanningRoutine("localhost:8080/planning", this.getPlanningResponseCallback));
+    }
+    /**
+     * Se hace un get a los pacientes para ver si ese código de Logueo existe
+     */
+    private IEnumerator getPlanningRoutine(string url, Action<string> callback = null)
+    {
+        var request = UnityWebRequest.Get(url);
+
+        yield return request.SendWebRequest();
+        var data = request.downloadHandler.text;
+
+        if (callback != null)
+            callback(data);
+    }
+
+    /**
+     * Una vez que obtiene los datos del paciente se modifica el json settings.json para que no pida nuevamente el logueo
+     */
+    private void getPlanningResponseCallback(string data) { }
+
 }
