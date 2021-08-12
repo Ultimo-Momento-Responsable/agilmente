f33a1eea698fab720e98aa7dd4965c77dbba395a
rassyu <andresmatter@hotmail.com>
2021-08-12 18:04:50 -0300

    :sparkles: Feat (main-menu): get planning work in progress

--- Assets/MainSceneController.cs
+++ Assets/MainSceneController.cs
@@ -10,16 +10,38 @@ public class MainSceneController : MonoBehaviour
     public Text title;
     public GameObject bodyText;
     private Settings settings;
+    public Button gameButton;
+    public Button profileButton;
+    public Button homeButton;
+    public Sprite[] homeSprite;
+    public Sprite[] profileSprite;
+    public Sprite[] gameSprite;
+    public Text gameName;
+
+    public void highlightButton(Image buttonImage, Sprite highlightedButton)
+    {
+        buttonImage.sprite = highlightedButton;
+    }
+
     public void btnGames()
     {
         title.text = "Jugar";
+        highlightButton(gameButton.GetComponent<Image>(), gameSprite[0]);
+        highlightButton(profileButton.GetComponent<Image>(), profileSprite[1]);
+        highlightButton(homeButton.GetComponent<Image>(), homeSprite[1]);
+
         bodyText.SetActive(true);
         bodyText.GetComponent<Text>().text = "Juegos pendientes"; 
         print("Boton jugar clickeado");
+
     }
     public void btnHome()
     {
         title.text = "Inicio";
+        highlightButton(gameButton.GetComponent<Image>(), gameSprite[1]);
+        highlightButton(profileButton.GetComponent<Image>(), profileSprite[1]);
+        highlightButton(homeButton.GetComponent<Image>(), homeSprite[0]);
+
         bodyText.SetActive(true);
         settings = JsonUtility.FromJson<Settings>(System.IO.File.ReadAllText(Application.dataPath + "/settings.json"));
         bodyText.GetComponent<Text>().text = "¡Hola de nuevo " + settings.Login.patient.firstName + "!";
@@ -28,6 +50,10 @@ public class MainSceneController : MonoBehaviour
     public void btnProfile()
     {
         title.text = "Perfil";
+        highlightButton(gameButton.GetComponent<Image>(), gameSprite[1]);
+        highlightButton(profileButton.GetComponent<Image>(), profileSprite[0]);
+        highlightButton(homeButton.GetComponent<Image>(), homeSprite[1]);
+
         bodyText.SetActive(false);
         print("Boton perfil clickeado");
     }
@@ -35,7 +61,8 @@ public class MainSceneController : MonoBehaviour
     //GET PARA PLANNING DESDE BACKEND - 
     public void getPlanning()
     {
-        this.StartCoroutine(this.getPlanningRoutine("localhost:8080/planning", this.getPlanningResponseCallback));
+        settings = JsonUtility.FromJson<Settings>(System.IO.File.ReadAllText(Application.dataPath + "/settings.json"));
+        this.StartCoroutine(this.getPlanningRoutine("localhost:8080/planning/patient_", this.getPlanningResponseCallback));
     }
     /**
      * Se hace un get a los pacientes para ver si ese código de Logueo existe
