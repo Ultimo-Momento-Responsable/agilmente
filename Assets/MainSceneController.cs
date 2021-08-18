fe7a52e240e3e7f7e13b3f69c9dc2196aa309655
rassyu <andresmatter@hotmail.com>
2021-08-18 20:56:01 -0300

    :sparkles: Feat (play-section): "Juegos" section done

--- Assets/MainSceneController.cs
+++ Assets/MainSceneController.cs
@@ -25,59 +25,67 @@ public class MainSceneController : MonoBehaviour
     public Camera camera;
     public Button gameCard;
     public GameObject gameCanvas;
+    public GameObject cardContainer;
 
-
+    /**
+     * Inicio de escena, genera una request que obtiene un JSON con los juegos pendientes asignados a una planificacion
+     */
     public void Start()
     {
         getPlanning();
     }
-    
-    //public void OnGUI()
-    //{
-    //    int i = 0;
 
-    //    Vector2 cardSize = camera.ViewportToWorldPoint(new Vector2(670, 175));
-    //    foreach (Planning planningCards in planningRequestJson.planningList)
-    //    {
-    //        Vector2 cardPosition = camera.ViewportToWorldPoint(new Vector2(0.0347f, 0.26f + (i * 0.15f)));
-    //        Rect card = new Rect(cardPosition, cardSize);
-    //        i++;
-    //        //Color cardColor = new Color(0f, 1f, 0f, 0.5f);
-    //        GUIStyle cardColor = new GUIStyle();
-    //        GUI.backgroundColor = Color.blue;
-    //        GUI.Button(card, "");
-    //    }
-
-    //}
+    /**
+     * Destruye todas las tarjetas actualmente creadas, utilizado para navegabilidad entre menus. 
+     */ 
+    public void clearCards()
+    {
+        foreach (GameObject cardsToDestroy in GameObject.FindGameObjectsWithTag("gameCard"))
+        {
+            Destroy(cardsToDestroy);
+        }
+    }
 
     public void highlightButton(Image buttonImage, Sprite highlightedButton)
     {
         buttonImage.sprite = highlightedButton;
     }
 
+    /**
+     * Muestra todos los elementos de la seccion "juegos" de la aplicacion
+     */
     public void btnGames()
     {
         title.text = "Jugar";
         gameName.text = planningRequestJson.planningList[0].game;
         numberOfSessions.text = "Quedan " + planningRequestJson.planningList[0].numberOfSession + " sesiones restantes";
 
+        clearCards();
+
         highlightButton(gameButton.GetComponent<Image>(), gameSprite[0]);
         highlightButton(profileButton.GetComponent<Image>(), profileSprite[1]);
         highlightButton(homeButton.GetComponent<Image>(), homeSprite[1]);
-        GameObject.FindGameObjectWithTag("gameCard").SetActive(false);
         bodyText.SetActive(true);
         bodyText.GetComponent<Text>().text = "Juegos pendientes"; 
         print("Boton jugar clickeado");
 
-        int i = 0;
 
+        /**
+         * Por cada juego pendiente, genera una card con la informacion del juego
+         * Define un contenedor scrolleable con tamaño igual a la cantidad de cards generadas
+         */
+
+        int i = 0;
+        cardContainer.GetComponent<RectTransform>().sizeDelta = new Vector2(cardContainer.GetComponent<RectTransform>().sizeDelta.x, cardContainer.GetComponent<RectTransform>().sizeDelta.y + (planningRequestJson.planningList.Length *1.5f));
         foreach (Planning planningCards in planningRequestJson.planningList)
         {
             Button gameCardInstance = Instantiate(gameCard);
             btnClickPlayGame(gameCardInstance, i);
-            gameCardInstance.transform.parent = gameCanvas.transform;
-            gameCardInstance.transform.localScale = new Vector2(1, 1);
-            gameCardInstance.transform.localPosition = new Vector3(0, 222.5f + (i*-222.5f), 0);
+            gameCardInstance.transform.parent = cardContainer.transform;
+            gameCardInstance.transform.localScale = new Vector2(0.0078f, 0.0078f);          //Escala actual del canvas
+            gameCardInstance.transform.localPosition = new Vector3(0, -1 + (i * -1.5f), 0); //Tamaño de cards + offset
+            print(4.45f + (i * -1.8f));
+
             foreach (Text gameName in gameCardInstance.GetComponentsInChildren<Text>())
             {
                 if (gameName.gameObject.name == "GameName")
@@ -95,42 +103,57 @@ public class MainSceneController : MonoBehaviour
 
     }
 
+    /**
+     * Llama al inicio del juego cuando se clickea en alguna de las cards
+     */
     void btnClickPlayGame(Button btnPlayGame, int index)
     {
         btnPlayGame.onClick.AddListener(() => playGame(index));
     }
 
-
-
+    /**
+     * Muestra todos los elementos de la seccion "Inicio" de la aplicacion
+     */
     public void btnHome()
     {
+        clearCards();
         title.text = "Inicio";
         highlightButton(gameButton.GetComponent<Image>(), gameSprite[1]);
         highlightButton(profileButton.GetComponent<Image>(), profileSprite[1]);
         highlightButton(homeButton.GetComponent<Image>(), homeSprite[0]);
+
 
         bodyText.SetActive(true);
         settings = JsonUtility.FromJson<Settings>(System.IO.File.ReadAllText(Application.dataPath + "/settings.json"));
         bodyText.GetComponent<Text>().text = "¡Hola de nuevo " + settings.Login.patient.firstName + "!";
         print("Boton inicio clickeado");
     }
+    
+    /**
+     * Muestra todos los elementos de la seccion "Perfil" de la aplicacion
+     */
     public void btnProfile()
     {
+        clearCards();
         title.text = "Perfil";
         highlightButton(gameButton.GetComponent<Image>(), gameSprite[1]);
         highlightButton(profileButton.GetComponent<Image>(), profileSprite[0]);
         highlightButton(homeButton.GetComponent<Image>(), homeSprite[1]);
 
+
         bodyText.SetActive(false);
         print("Boton perfil clickeado");
     }
 
-    //GET PARA PLANNING DESDE BACKEND - 
+    /**
+     * GET para planificaciones desde backend para un paciente
+     */
     public void getPlanning()
     {
         settings = JsonUtility.FromJson<Settings>(System.IO.File.ReadAllText(Application.dataPath + "/settings.json"));
         this.StartCoroutine(this.getPlanningRoutine("localhost:8080/planning/mobile_patient_" + settings.Login.patient.id, this.getPlanningResponseCallback));
     }
+
     /**
      * Se hace un get a los pacientes para ver si ese código de Logueo existe
      */
@@ -146,18 +169,19 @@ public class MainSceneController : MonoBehaviour
     }
 
     /**
-     * Una vez que obtiene los datos del paciente se modifica el json settings.json para que no pida nuevamente el logueo
+     * Callback utilizado en getPlanning(), asigna a variables de Unity los datos obtenidos del JSON
      */
     private void getPlanningResponseCallback(string data) { 
-        //recorrer con un for, para Guille es solo el primero
         planningRequestJson = JsonUtility.FromJson<PlanningList>(data);
         gameName.text = planningRequestJson.planningList[0].game;
         numberOfSessions.text = "Quedan " + planningRequestJson.planningList[0].numberOfSession + " sesiones restantes";
     }
 
+    /**
+     * Ejecuta una instancia de juego segun los parametros definidos en la planificacion
+     */
     public void playGame(int index)
     {
-        print("llega a ejecutar Play Game");
         if (planningRequestJson.planningList[index].game == "Encuentra al Repetido")
         {
             if (planningRequestJson.planningList[index].parameters[0].name=="figureQuantity")
@@ -180,11 +204,7 @@ public class MainSceneController : MonoBehaviour
         public Params[] parameters;
 
     }
-    //recibir del JSON para planningRequest
-    //al data PlanningRequest.Planning[i].numberOfSessions
-    //al data PlanningRequest.Planning[i].name
-    //Para guille es solo el mas proximo/primero
-    //crear variable del tipo PlanningRequest con nombre
+
     [System.Serializable]
     public class PlanningList
     {
