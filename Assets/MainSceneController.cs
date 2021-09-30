e11e695bbfaa573c0f124019ad9032be78d956d5
Fenlethmarg <guillermo.gribaudo95@gmail.com>
2021-09-30 17:12:34 -0300

    :bug: Fix (Param): fix bug with distractors

--- Assets/MainSceneController.cs
+++ Assets/MainSceneController.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
