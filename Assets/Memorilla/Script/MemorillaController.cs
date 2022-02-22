e0862935780dd0b7d8ff6e4e2de42d213f0a1184
ArmatorX <17768182+ArmatorX@users.noreply.github.com>
2022-02-22 19:45:43 -0300

    :sparkles: Feat (Memorilla): Add levels logic and add animations

--- Assets/Memorilla/Script/MemorillaController.cs
+++ Assets/Memorilla/Script/MemorillaController.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class MemorillaController : GameController
 {
     [SerializeField]
-    private int height = 4;
+    private int height = 7;
     [SerializeField]
-    private int width = 4;
+    private int width = 5;
     [SerializeField]
     private Vector3 offset;
     [SerializeField]
@@ -22,6 +22,7 @@ public class MemorillaController : GameController
     private float cellSpaceBetweenRows = 0.1f;
     private List<List<Cell>> grid;
     private bool controlsEnabled;
+    private int numberOfGuesses;
 
     public int Height { get => height; }
     public int Width { get => width; }
@@ -30,22 +31,23 @@ public class MemorillaController : GameController
     public float CellSize { get => cellSize; }
     public int NumberOfStimuli { get => numberOfStimuli; }
     public int NumberOfLevels { get => numberOfLevels; }
-    public bool ControlsEnabled { get => controlsEnabled; set => controlsEnabled = value; }
+    public bool ControlsEnabled { get => controlsEnabled; }
+    public float CellSpaceBetweenColumns { get => cellSpaceBetweenColumns; set => cellSpaceBetweenColumns = value; }
+    public float CellSpaceBetweenRows { get => cellSpaceBetweenRows; set => cellSpaceBetweenRows = value; }
+    public int NumberOfGuesses { get => numberOfGuesses; set => numberOfGuesses = value; }
 
     public GameObject CellPrefab;
     public GameObject GridGameObject;
 
     void Start()
     {
-        takeControlFromPlayer();
+        TakeControlFromPlayer();
         cellSize = CellPrefab.gameObject.transform.localScale.x;
-        float originY = -(Height * CellSize + (Height - 1) * cellSpaceBetweenRows) / 2;
+        float originY = -(Height * CellSize + (Height - 1) * CellSpaceBetweenRows) / 2;
         GridGameObject.transform.position = new Vector3(GridGameObject.transform.position.x, originY);
         CreateGrid();
         StartLevel();
     }
-
-
 
     void Update()
     {
@@ -77,14 +79,22 @@ public class MemorillaController : GameController
         throw new System.NotImplementedException();
     }
 
+    /// <summary>
+    /// Inicializa el nivel actual.
+    /// </summary>
     private void StartLevel()
     {
+        NumberOfGuesses = NumberOfStimuli;
         CleanGrid();
         CreateStimuli();
-        StartCoroutine(StartCountdown());
-        ReturnControlToPlayer();
+        StartCoroutine(WaitWhileShowingSolution(5));
     }
 
+    /// <summary>
+    /// Limpia la grilla de estados y colores.
+    /// Reinicia todas las celdas al estado UNSELECTED,
+    /// y también setea el isActive a false.
+    /// </summary>
     private void CleanGrid()
     {
         foreach (List<Cell> row in Grid)
@@ -95,6 +105,11 @@ public class MemorillaController : GameController
             }
         }
     }
+
+    /// <summary>
+    /// Selecciona n celdas random para el ejercicio
+    /// las cuales conformarán la solución del mismo.
+    /// </summary>
     private void CreateStimuli()
 
     {
@@ -103,30 +118,36 @@ public class MemorillaController : GameController
             while (true) {
                 int randomX = Random.Range(0, Width);
                 int randomY = Random.Range(0, Height);
-                Cell selectedCell = Grid[randomX][randomY];
+                Cell selectedCell = Grid[randomY][randomX];
 
                 if (!selectedCell.IsActive)
                 {
                     selectedCell.IsActive = true;
                     selectedCell.State = STATES.SELECTED;
-                    Debug.Log("El espacio " + randomX + " " + randomY + " fue asignado como activo");
 
                     break;
                 }
-                Debug.Log("El espacio ya se encuentra ocupado, rerolleando");
             }
-            // Elegir un (randomX, RandomY) asignarle el cuadro, si esta usado, rerollear
-            Debug.Log("asignacion de espacio numero: " + i);
         }
     }
-    private IEnumerator StartCountdown()
+
+    /// <summary>
+    /// Inicia una corrutina para esperar 5 segundos
+    /// antes de ocultar la solución.
+    /// </summary>
+    /// <param name="seconds">Cantidad de segundos a esperar.</param>
+    /// <returns>Una corrutina.</returns>
+    private IEnumerator WaitWhileShowingSolution(float seconds)
     {
-        Debug.Log("Esperando 5 segundos antes de comenzar");
-        yield return new WaitForSeconds(5);
-        Debug.Log("Comienzo de juego");
+        yield return new WaitForSeconds(seconds);
         HideStimuli();
         ReturnControlToPlayer();
     }
+
+    /// <summary>
+    /// Esconde los estímulos (la solución).
+    /// Cambia el estado de todas las seldas a UNSELECTED.
+    /// </summary>
     private void HideStimuli()
     {
         foreach (List<Cell> row in Grid)
@@ -137,12 +158,23 @@ public class MemorillaController : GameController
             }
         }
     }
+
+    /// <summary>
+    /// Devuelve el control al jugador.
+    /// Permite al jugador seleccionar celdas de la
+    /// grilla.
+    /// </summary>
     private void ReturnControlToPlayer()
     {
         controlsEnabled = true;
     }
 
-    private void takeControlFromPlayer()
+    /// <summary>
+    /// Quita el control al jugador.
+    /// Evita que el jugador seleccione celdas de la
+    /// grilla.
+    /// </summary>
+    private void TakeControlFromPlayer()
     {
         controlsEnabled = false;
     }
@@ -175,9 +207,56 @@ public class MemorillaController : GameController
     private Cell CreateCell(int row, int column)
     {
         GameObject cellGameObject = Instantiate(CellPrefab, GridGameObject.transform);
-        Cell cell = new Cell(row, column, cellGameObject);
-        //cellGameObject.transform.localPosition = new Vector3(column * (CellSize + cellSpaceBetweenColumns), row * (CellSize + cellSpaceBetweenRows), 0);
-        cellGameObject.transform.localPosition = new Vector3(column * (CellSize + cellSpaceBetweenColumns), row * (CellSize + cellSpaceBetweenRows), 0);
+        Cell cell = cellGameObject.GetComponent<Cell>();
+        cell.Create(row, column, this);
+        cellGameObject.transform.localPosition = new Vector3(cell.PosX, cell.PosY, 0);
         return cell;
+    }
+
+    /// <summary>
+    /// Evento que gestiona lo que ocurre cuando se clickea
+    /// una celda de la grilla.
+    /// </summary>
+    public void OnCellClicked()
+    {
+        NumberOfGuesses--;
+
+        if (NumberOfGuesses == 0)
+        {
+            TakeControlFromPlayer();
+            ShowSolution();
+            StartNextLevel();
+        }
+    }
+
+    private void StartNextLevel()
+    {
+        StartCoroutine(WaitBeforeNextLevel(3));
+    }
+
+    /// <summary>
+    /// Inicia una corrutina para esperar seconds segundos
+    /// antes de pasar al siguiente nivel.
+    /// </summary>
+    /// <param name="seconds">Cantidad de segundos a esperar.</param>
+    /// <returns>Una corrutina.</returns>
+    private IEnumerator WaitBeforeNextLevel(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        StartLevel();
+    }
+
+    /// <summary>
+    /// Muestra la solucion correcta y los errores cometidos.
+    /// </summary>
+    private void ShowSolution()
+    {
+        foreach (List<Cell> row in Grid)
+        {
+            foreach (Cell cell in row)
+            {
+                cell.CheckSolution();
+            }
+        }
     }
 }
--- Assets/Memorilla/Script/Models/Cell.cs
+++ Assets/Memorilla/Script/Models/Cell.cs
@@ -1,64 +1,96 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 public enum STATES
 {
     UNSELECTED,     // Esta celda no fue seleccionada para ser activa, color default
     SELECTED,       // Esta celda fue seleccionada para ser activa, color ROJO?
-    MISSELECTED,    // Esta celda fue seleccionada pero no es activa, color ERROR
     MISS,           // Esta celda no fue seleccionada pero es activa, color so boludo
     CORRECT,        // Esta celda fue seleccionada y es activa, color verde
 }
-public class Cell
+public class Cell: MonoBehaviour
 {
     private int row;
     private int column;
-    private GameObject gameObject;
     private STATES state;
     private bool isActive;
-    public Cell(int row, int column, GameObject gameObject)
-    {
-        this.row = row;
-        this.column = column;
-        this.isActive = false;
-        this.gameObject = gameObject;
-        State = STATES.UNSELECTED;
-    }
+    private MemorillaController controller;
 
+    public float PosX { get => column * (controller.CellSize + controller.CellSpaceBetweenColumns); }
+    public float PosY { get => row * (controller.CellSize + controller.CellSpaceBetweenRows); }
     public int Row { get => row; set => row = value; }
     public int Column { get => column; set => column = value; }
-    public GameObject GameObject { get => gameObject; set => gameObject = value; }
     public STATES State
     {
         get => state; 
         set
         {
-            Color color;
-            switch (value)
-            {
-                case STATES.SELECTED:
-                    color = new Color(0, 0, 1, 1);
-                    break;
-                case STATES.MISSELECTED:
-                    color = new Color(1, 0, 0, 1);
-                    break;
-                case STATES.MISS:
-                    color = new Color(1, 0, 0, 1);
-                    break;
-                case STATES.CORRECT:
-                    color = new Color(0, 1, 0, 1);
-                    break;
-                default:
-                    color = new Color(1, 1, 1, 1);
-                    break;
-            }
             state = value;
-            gameObject.GetComponentInChildren<Image>().color = color;
+            gameObject.GetComponentInChildren<Animator>().SetInteger("State", (int) value);
         }
     }
-    public bool IsActive { get => isActive; set => isActive = value; } 
+    public bool IsActive { get => isActive; set => isActive = value; }
+
+    /// <summary>
+    /// Inicializa la celda.
+    /// </summary>
+    /// <param name="row">Fila donde se encuentra la celda.</param>
+    /// <param name="column">Columna donde se encuentra la celda.</param>
+    /// <param name="controller">Controlador del juego.</param>
+    /// <returns>La instancia de la celda.</returns>
+    public Cell Create(int row, int column, MemorillaController controller)
+    {
+        this.row = row;
+        this.column = column;
+        this.isActive = false;
+        this.controller = controller;
+        State = STATES.UNSELECTED;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Limpia la celda.
+    /// Pasa el estado a UNSELECTED y
+    /// si tiene guardada una respuesta, la borra.
+    /// </summary>
     public void Clear() {
         IsActive = false;
         State = STATES.UNSELECTED;
-    }    
+    }
+
+    /// <summary>
+    /// Evento que gestiona lo que ocurre cuando 
+    /// se clickea la celda.
+    /// </summary>
+    public void OnCellClick()
+    {
+        if (!controller.ControlsEnabled)
+            return;
+
+        if (State == STATES.UNSELECTED)
+        {
+            State = STATES.SELECTED;
+            controller.OnCellClicked();
+        }
+    }
+
+    /// <summary>
+    /// Verifica si el jugador seleccionó la opción
+    /// correcta.
+    /// </summary>
+    public void CheckSolution()
+    {
+        if (IsActive && State == STATES.SELECTED)
+        {
+            State = STATES.CORRECT;
+        } else if (IsActive && State == STATES.UNSELECTED)
+        {
+            State = STATES.MISS;
+        } else if (!IsActive && State == STATES.SELECTED)
+        {
+            State = STATES.UNSELECTED;
+        }
+    }
 }
 
