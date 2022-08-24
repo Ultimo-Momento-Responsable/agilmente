3ecfc4456663438a02e57c9b5a39087740b05d90
Fenlethmarg <guillermo.gribaudo95@gmail.com>
2022-08-24 20:03:58 -0300

    :bug: Fix (Memorilla): fix tutorial hands position

--- Assets/Memorilla/Script/MemorillaController.cs
+++ Assets/Memorilla/Script/MemorillaController.cs
@@ -138,6 +138,7 @@ public class MemorillaController : GameController
     /// </summary>
     private void CreateTutoHands()
     {
+        var k = 2;
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
@@ -150,11 +151,14 @@ public class MemorillaController : GameController
                     GameObject tHand = Instantiate(handPref, GridGameObject.transform);
                     tHand.GetComponent<Transform>().localScale = new Vector3(CellSize/5, CellSize/5, 1);
                     tHand.transform.localPosition = new Vector3(selectedCell.PosX+90f, 0);
-                    tHand.GetComponent<TutorialHand>().yPos = (selectedCell.PosY/100-0.35f)-0.85f;
+                    print(selectedCell.PosY);
+                    tHand.GetComponent<TutorialHand>().yPos = -0.8f-k*1.6f;
+                    print(tHand.GetComponent<TutorialHand>().yPos);
                     tHand.SetActive(true);
                     tutorialHands.Add(tHand);
                 }
             }
+            k--;
         }
     }
 
