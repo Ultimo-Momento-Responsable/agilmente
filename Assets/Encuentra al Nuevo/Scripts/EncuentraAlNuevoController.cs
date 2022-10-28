acbc8fd6e48460dd03f8f2f81a23cb21d76f6cd5
julianp950 <julian.p950@gmail.com>
2022-10-28 17:59:05 -0300

    :wrench: Chore: remove superficial code

--- Assets/Encuentra al Nuevo/Scripts/EncuentraAlNuevoController.cs
+++ Assets/Encuentra al Nuevo/Scripts/EncuentraAlNuevoController.cs
@@ -97,7 +97,6 @@ public class EncuentraAlNuevoController : GameController, ControllerWithFigureBe
     {
         if (encuentraAlNuevo.onTutorial)
         {
-            pauseButton.SetActive(true);
             timer.text = "Tutorial";
             if (isTouching && !prevTutorial)
             {
